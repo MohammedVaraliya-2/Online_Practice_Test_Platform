cc8d50e2935f51311dd28aae5465b9127ca5dd46
MohammedVaraliya-2 <varaliyamohammed1@gmail.com>
2024-05-31 15:27:07 +0530

    Updated Quiz Completed component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import "./globals.css";
 import Login from "./components/_auth/Login";
 import Dashboard from "./components/Dashboard";
 import Profile from "./components/Profile";
+import { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <>
       <main className="min-h-screen bg-dark-1">
